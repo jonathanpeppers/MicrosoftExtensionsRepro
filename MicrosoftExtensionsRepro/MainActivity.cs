ae5668fd4b398f7f98e482c22669b9c3df4bd23a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-08-24 09:43:19 -0500

    Microsoft.Extensions.DependencyInjection usage

--- MicrosoftExtensionsRepro/MainActivity.cs
+++ MicrosoftExtensionsRepro/MainActivity.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace MicrosoftExtensionsRepro;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
@@ -9,5 +11,30 @@ public class MainActivity : Activity
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
+
+        // Microsoft.Extensions thingies
+        var provider = new ServiceCollection()
+            .AddSingleton<IFoo>(_ => new FooService())
+            .BuildServiceProvider();
+
+        var foo = provider.GetService<IFoo>();
+        if (foo != null)
+        {
+            foo.DoStuff();
+        }
+        else
+        {
+            Console.WriteLine("foo is null!");
+        }
     }
+}
+
+interface IFoo
+{
+    void DoStuff();
+}
+
+class FooService : IFoo
+{
+    public void DoStuff() => Console.WriteLine("Did it!");
 }
